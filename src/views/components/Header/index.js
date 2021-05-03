fb74868e163eeb36ae775c455baf1cad93dff755
Lucas Campanelli <lucas.campanelli@outlook.com>
2021-05-03 02:28:32 -0300

    Estilo header

--- src/views/components/Header/index.js
+++ src/views/components/Header/index.js
@@ -3,13 +3,13 @@ import React from 'react';
 export default function Header(){
     return(
         <header>
-            <img src=''/>
-            <nav>
-                <ul>
-                    <li>Início</li>
-                    <li>Portfólio</li>
-                    <li>Serviços</li>
-                    <li>Orçamentos</li>
+            <img src='mk_logo1.svg' alt='Logo da MK Studio.' className='logoHeader'/>
+            <nav class='navigation'>
+                <ul className='menuList'>
+                    <li className='menuOption'>Início</li>
+                    <li className='menuOption'>Portfólio</li>
+                    <li className='menuOption'>Serviços</li>
+                    <li className='menuOption'>Orçamentos</li>
                 </ul>
             </nav>
         </header>
