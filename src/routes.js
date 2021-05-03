18010673a4fc1919dd458b16578de09df8f2aa2b
Lucas Campanelli <lucas.campanelli@outlook.com>
2021-05-03 00:47:14 -0300

    Rotas

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+export default function Routes() {
+    <BrowserRouter>
+        <Switch>
+
+            <Route exact path='/' component={} />
+            <Route path='/portfolio' component={} />
+            <Route path='/orcamento' component={} />
+            <Route path='/servicos' component={} />
+
+        </Switch>
+    </BrowserRouter>
+}
