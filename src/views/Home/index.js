5c3e195b40411cca58f6e79fadd3a0fc578cb699
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2021-05-03 02:28:57 -0300

    Iniciando Home

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
+import './index.css';
 
 export default function Home(){
     return(
-        <div>
-            <Header/>
-            <h1>home</h1>
-        </div>
+        <>
+        <Header/>
+        <section className="firstContact">
+            <h1>MK Studio</h1>
+            <h2>Uma página na web é o que você precisa para ter sucesso!</h2>
+        </section>
+        <Footer/>
+        </>
     );
 }
