4bf9f657ce3de2597e6ae1c8eed0c65db31706ee
Lucas Campanelli <lucas.campanelli@outlook.com>
2021-05-03 01:05:16 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import './global.css';
+import Routes from './routes.js';
+import Header from './views/components/Header';
+import Footer from './views/components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+        <Routes />
+      <Footer/>
+    </>
   );
 }
 
---
+++ src/views/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Header(){
+    return(
+        <header>
+            <img src=''/>
+            <nav>
+                <ul>
+                    <li>Início</li>
+                    <li>Portfólio</li>
+                    <li>Serviços</li>
+                    <li>Orçamentos</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
