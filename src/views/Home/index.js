849620b48afd3cefb5a872bd37e37a7b03ad199a
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2021-05-03 17:53:07 -0300

    Adicionando Sistema de Opacidade do Header

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import './index.css';
 
 export default function Home(){
+
+    const [headerOp, setHeaderOp] = useState('00')
+
+    window.onscroll = () => {
+        const scr = document.documentElement.scrollTop;
+        if(scr > 20){ setHeaderOp('FF'); }
+        else if(scr <= 20){ setHeaderOp('00'); }
+    }
+
     return(
         <>
-        <Header selected="home" bgOpacity="00"/>
+        <Header selected="home" bgOpacity={headerOp} />
         <section className="firstContact">
             <h1 className="firstContactTitle">MK Studio</h1>
             <h2 className="firstContactSubtitle">Uma página na web é o que você precisa para ter sucesso.</h2>
