68f59e00c99805bea9cd7cbbca4baeca7435ec75
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2021-05-03 23:39:31 -0300

    Fazendo Textos da tela inicial

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -9,17 +9,34 @@ export default function Home(){
 
     window.onscroll = () => {
         const scr = document.documentElement.scrollTop;
-        if(scr > 20){ setHeaderOp('FF'); }
-        else if(scr <= 20){ setHeaderOp('00'); }
+        if(scr > 40){ setHeaderOp('FF'); }
+        else if(scr <= 40){ setHeaderOp('00'); }
     }
 
     return(
         <>
         <Header selected="home" bgOpacity={headerOp} />
-        <section className="firstContact">
-            <h1 className="firstContactTitle">MK Studio</h1>
-            <h2 className="firstContactSubtitle">Uma página na web é o que você precisa para ter sucesso.</h2>
-        </section>
+        <main>
+            <article>
+                <section className="firstContact">
+                    <h1 className="firstContactTitle">MK Studio</h1>
+                    <h2 className="firstContactSubtitle">Nós construímos páginas para levar você ao seu sucesso!</h2>
+                    <button className="firstContactButton">Veja como</button>
+                </section>
+                <section className="mainSection">
+                    <h2 className="mainSectionTitle">Seja qual for o objetivo</h2>
+                    <h3 className="mainSectionSubitle">Um site vai te dar o impulso que você precisa para ir ainda mais longe. 🚀</h3>
+                    <p className="mainSectionParagraph">Para vender mais, conquistar mais seguidores, ou obter clientes a porta de entrada do negócio precisa ser: acessível, informativa, bonita e o mais importante de tudo... digital!</p>
+                    <p className="mainSectionParagraph">Todo mundo gosta de praticidade e agilidade, e nós proporcionamos isso.</p>
+                </section>
+                <section className="mainSection">
+                    <h2 className="mainSectionTitle">Percebeu alguma diferença?</h2>
+                    <h3 className="mainSectionSubitle">Nossas páginas são mais rápidas e otimizadas. 🏃‍♂️</h3>
+                    <p className="mainSectionParagraph">Uma página rápida significa maior interesse dos visitantes, que não perdem tempo ao navegar pelo seu site. Tudo direto ao ponto, sem travamentos, páginas congeladas e carregamentos demorados.</p>
+                    <p className="mainSectionParagraph">Já uma página otimizada representa uma página limpa com tudo o que o cliente precisa ao alcance dos olhos.</p>
+                </section>
+            </article>
+        </main>
         <Footer/>
         </>
     );
