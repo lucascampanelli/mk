094ae8768ca11a890240847263a47387fd00d175
Lucas Campanelli <lucas.campanelli@outlook.com>
2021-05-04 00:15:32 -0300

    Página de contato

--- src/views/Orcamento/index.js
+++ src/views/Orcamento/index.js
@@ -20,12 +20,35 @@ export default function Orcamento(){
                     <h2 className="titleBlack">Fale um pouco sobre o seu negócio</h2>
                     <div className='contactContent'>
                         <div className="leftContact">
-                            <p></p>
+                            <p className="contactDesc">Para que possamos realizar um serviço de excelência, cumprindo todos os requisitos do seu negócio, é necessário que você fale detalhadamente sobre a sua necessidade.</p>
+                            <p className="contactDesc">Assim que recebermos o seu pedido de orçamento, analisaremos o caso e prontamente responderemos por e-mail ou telefone, como você preferir :)</p>
+                            <p className="contactDesc">Fique tranquilo, o orçamento é sem compromisso e sem custo algum!</p>
                         </div>
                         <div className="rightContact">
                             <div className="formArea">
+                                <h3>Conte-nos sobre o que precisa</h3>
+                                <p className="h3Desc">A MK Studio possui experiência para te ajudar no que precisar! Conte detalhadamente sobre sua necessidade.</p>
                                 <form>
+                                    <div className="inputGrid">
+                                        <input type="text" name="name" className="defaultInputGrid" placeholder="Nome" />
+                                        <input type="text" name="name" className="defaultInputGrid" placeholder="Sobrenome"/>
+                                        <input type="email" name="name" className="defaultInputGrid" placeholder="E-mail" />
+                                        <input type="tel" name="name" className="defaultInputGrid" placeholder="Telefone" />
+                                        <input type="text" name="name" className="defaultInputGrid" placeholder="Empresa" />
+                                    </div>
                                     
+                                    <div className="messageDiv">
+                                        <label id="labelWhatsapp">Gostaria que entrássemos em contato por Whatsapp?</label>
+                                        <div className="radioGroup">
+                                            <input type="radio" name="whatsapp" id="nao" value="nao" />
+                                            <label for="nao">Não</label>
+                                            <input type="radio" name="whatsapp" id="sim" value="sim" checked/>
+                                            <label for="sim">Sim</label>
+                                        </div>
+                                        <textarea name="name" className="defaultInput" placeholder="Fale sobre sua necessidade"/>
+
+                                        <button className="btnEnviar">ENVIAR</button>
+                                    </div>
                                 </form>
                             </div>
                         </div>
